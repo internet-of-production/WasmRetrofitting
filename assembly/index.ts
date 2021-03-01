dc0473dd103b3eac2a035c1d41abd3ddedb447f6
Otoya Nakakaze <otoya.nakakaze@rwth-aachen.de>
2021-03-01 11:06:09 +0100

    Add const variable for data of the axis

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,7 @@
 //Create a static array for data
 let axisData = new Uint8Array(1024);
+//The length of axis data is 9 bytes
+const AXIS_DATA_LENGTH = 9;
 
 
 
@@ -8,10 +10,10 @@ export function add(a: i32, b: i32): i32 {
 }
 
 //It counts the number of data of Axis 1.
-//The length of axis data is 9 bytes, and 1st byte is the id of axes.
+//1st byte is the id of axes.
 export function countFirstAxisData(numberOfData:i32):i32{
   let counter:i32 = 0;
-  for(let i:i32 = 0; i<numberOfData*9; i=i+9){
+  for(let i:i32 = 0; i<numberOfData*AXIS_DATA_LENGTH; i=i+AXIS_DATA_LENGTH){
 
     //load data from the shared memory, and check the value
     if(load<u8>(i)==1){
